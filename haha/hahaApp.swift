1ca18c64c3220a0dfb63fa9d540fc55153ff16ce
oniszczak <aleks42@gmail.com>
2023-01-07 23:31:50 -0500

    Initial Commit

---
+++ haha/hahaApp.swift
@@ -0,0 +1,17 @@
+//
+//  hahaApp.swift
+//  haha
+//
+//  Created by ALEKSANDER ONISZCZAK on 2023-01-07.
+//
+
+import SwiftUI
+
+@main
+struct hahaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
